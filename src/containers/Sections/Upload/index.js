75d77afde7a8a00beb7256498ee779019edbc87d
talentdev2020 <48382274+talentdev2020@users.noreply.github.com>
2021-07-15 17:50:50 -0700

    implement dataOrder to compare multi level based on Metada

--- src/containers/Sections/Upload/index.js
+++ src/containers/Sections/Upload/index.js
@@ -1,63 +1,126 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
+import { Col, Divider, Form, Row, Switch } from 'antd'
 import { get } from 'lodash'
-import { selectAnalysisType } from 'store/modules/analyses'
+import {
+  listUploadableStudy,
+  selectUploadableStudies,
+  selectSitesStatus,
+  LIST_UPLOADABLE_STUDY,
+} from 'store/modules/sites'
+import { initializeCurrentFiles, setAllFiles, setCurrentFiles, updateCurrentFileFields } from 'store/modules/datafiles'
+import { DataFileTree, Loader, MetadataEditor } from 'components'
+import { selectAnalysisType, selectAnalysis } from 'store/modules/analyses'
 
-import DefaultUploadSection from './DefaultUpload'
-import DFNCUploadSection from './dFNC'
-import ASLUploadSection from './ASL'
-import MancovaUploadSection from './Mancova'
-import FMRI32UploadSection from './fMRI32'
-import GroupICAUploadSection from './GICA'
-import GroupSPMGLMUploadSection from './GroupSPMGLM'
-import SPMGLMUploadSection from './SPMGLM'
-import WMHUploadSection from './WMH'
-import FreesurferRegressionUploadSection from './FreesurferRegression'
+const { Item: FormItem } = Form
 
-const UploadSection = ({ analysisType }) => {
-  function getUploadSection() {
-    const analysisLabel = get(analysisType, 'label')
+export const GroupICAUploadSection = props => {
+  const {
+    analysisType,
+    analysis,
+    updateCurrentFileFields,
+    setAllFiles,
+    setCurrentFiles,
+    showSelector,
+    sitesStatus,
+  } = props
 
-    switch (analysisLabel) {
-      case 'ASL':
-        return <ASLUploadSection />
-      case 'dFNC':
-        return <DFNCUploadSection />
-      case 'fMRI_32ch':
-        return <FMRI32UploadSection />
-      case 'FSR':
-        return <FreesurferRegressionUploadSection />
-      case 'GICA':
-        return <GroupICAUploadSection />
-      case 'GroupSPMGLM':
-        return <GroupSPMGLMUploadSection />
-      case 'MANCOVA':
-        return <MancovaUploadSection />
-      case 'SPMGLM':
-        return <SPMGLMUploadSection />
-      case 'WMH':
-        return <WMHUploadSection />
-      default:
-        return <DefaultUploadSection />
-    }
+  const [dataOrder, setDataOrder] = useState([])
+  const [isMetadataSelected, setIsMetadataSelected] = useState(false)
+
+  const inputFile = get(analysis, 'parameters.analysis.options.files.value')
+
+  const handleTreeChange = files => {
+    setAllFiles(files)
+    setCurrentFiles(files)
+  }
+
+  const loading = () => {
+    return sitesStatus === LIST_UPLOADABLE_STUDY
+  }
+
+  if (loading()) {
+    return <Loader />
   }
 
   return (
-    <div className="analysis-start-widget">
-      <div className="analysis-start-widget-heading">Data Sets</div>
-      {getUploadSection()}
-    </div>
+    <Row>
+      <Col style={{ textAlign: 'right' }}>
+        <Switch
+          checkedChildren="GICA Metadata"
+          unCheckedChildren="GICA Metadata"
+          checked={isMetadataSelected}
+          onChange={setIsMetadataSelected}
+        />
+      </Col>
+      <Col>
+        {isMetadataSelected && (
+          <Fragment>
+            <MetadataEditor
+              allowedHeaders={['subject', 'session', 'series', 'datafile']}
+              onChange={metadata => setDataOrder(get(metadata, 'results'))}
+            />
+            <Divider style={{ marginTop: 36 }} />
+          </Fragment>
+        )}
+      </Col>
+      {showSelector && (
+        <Col>
+          <FormItem
+            label={`${isMetadataSelected ? 'And select' : 'Select'} a previously uploaded/output dataset:`}
+            style={{ fontSize: 18 }}
+          >
+            <DataFileTree
+              multiple
+              dataOrder={dataOrder}
+              analysisType={analysisType}
+              analysis={analysis}
+              initialValue={inputFile}
+              onChange={handleTreeChange}
+              onUpdateFields={updateCurrentFileFields}
+            />
+          </FormItem>
+        </Col>
+      )}
+    </Row>
   )
 }
 
-UploadSection.propTypes = {
+GroupICAUploadSection.propTypes = {
+  analysis: PropTypes.object,
+  studies: PropTypes.array,
+  sitesStatus: PropTypes.string,
   analysisType: PropTypes.object,
+  showSelector: PropTypes.bool,
+  listUploadableStudy: PropTypes.func,
+  setAllFiles: PropTypes.func,
+  setCurrentFiles: PropTypes.func,
+  updateCurrentFileFields: PropTypes.func,
+  initializeCurrentFiles: PropTypes.func,
+}
+
+GroupICAUploadSection.defaultProps = {
+  showSelector: true,
 }
 
 const selectors = createStructuredSelector({
+  analysis: selectAnalysis,
   analysisType: selectAnalysisType,
+  studies: selectUploadableStudies,
+  sitesStatus: selectSitesStatus,
 })
 
-export default connect(selectors)(UploadSection)
+const actions = {
+  initializeCurrentFiles,
+  listUploadableStudy,
+  setCurrentFiles,
+  setAllFiles,
+  updateCurrentFileFields,
+}
+
+export default connect(
+  selectors,
+  actions,
+)(GroupICAUploadSection)
