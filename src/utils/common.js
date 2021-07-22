8e9662308c3386050d2ad3259526d48f6c4e2ddd
talentdev2020 <48382274+talentdev2020@users.noreply.github.com>
2021-07-21 19:34:45 -0700

    implement data diff function

--- src/utils/common.js
+++ src/utils/common.js
@@ -83,3 +83,9 @@ export const runPromisesAsync = () => {
     }, 0)
   })
 }
+
+export const date_diff_indays = (date) => {
+	const dt1 = new Date(date);
+	const dt2 = new Date();
+	return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+}
